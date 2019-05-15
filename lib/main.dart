34adf073dc677f43d031c5c9f4c1cefcef83994a
Trịnh Quốc Thông <thong21012@gmail.com>
2019-05-15 23:41:41 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Hello Thông Trịnh', style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Text(
               '$_counter',
