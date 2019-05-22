c9c5260f311da5928a8fa9aadd083fa980c5b99c
danghoangkhiem <danghoangkhiem@hotmail.com>
2019-05-22 09:49:22 +0700

    add invoice detail page

---
+++ lib/src/ui/pages/invoice/invoice_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InvoiceDetailPage extends StatefulWidget {
+  @override
+  _InvoiceDetailPageState createState() => _InvoiceDetailPageState();
+}
+
+class _InvoiceDetailPageState extends State<InvoiceDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
