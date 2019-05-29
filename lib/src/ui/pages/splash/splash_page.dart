5f0000eeb1353286b4875d670ceacdf22b87ce68
Trinh Quoc Thong <thong21012@gmail.com>
2019-05-29 10:36:34 +0700

    update splash screen by thong

--- lib/src/ui/pages/splash/splash_page.dart
+++ lib/src/ui/pages/splash/splash_page.dart
@@ -3,17 +3,24 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Align(
-          alignment: Alignment.center,
-          child: CircularProgressIndicator(),
-        ),
-        ModalBarrier(
-          dismissible: false,
-          color: Colors.black.withOpacity(0.3),
-        ),
-      ],
+    return Container(
+      color: Colors.blueAccent,
+      child: new Center(
+        child: new Image.asset("assets/images/logo.png", width: 200,),
+      ),
     );
   }
 }
+
+//Stack(
+//children: <Widget>[
+//Align(
+//alignment: Alignment.center,
+//child: CircularProgressIndicator(),
+//),
+//ModalBarrier(
+//dismissible: false,
+//color: Colors.black.withOpacity(0.3),
+//),
+//],
+//)
